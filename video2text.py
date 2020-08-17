1e603eaf68427bf60db9c0e81056af3db338be95
Vihas3345 <55677411+Vihas3345@users.noreply.github.com>
2020-08-17 20:53:15 +0530

    Create video2text.py

---
+++ video2text.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+import moviepy.editor as mp
+clip=mp.VideoFileClip("video.mp4")  
+
+# wav format is better suited for speech recognition module
+
+clip.audio.write_audiofile("converted.wav")
+r=sr.Recognizer()
+audio=sr.AudioFile("converted.wav")
+with audio as source:
+    audio_file=r.record(source)
+result=r.recognize_google(audio_file)
+with open('recognized.txt',mode ='w') as file: 
+   file.write("Recognized Speech:") 
+   file.write("\n") 
+   file.write(result) 
+   print("ready!")
